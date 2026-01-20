46a4a814b73a647410ae08a53754cddad9b36dae
DishaAndre <andredisha55@gmail.com>
2026-01-20 12:25:46 +0530

    add application entry point and root App component

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./styles/tailwind.css";
+import "./styles/index.css";
+import "./utils/seedMoodData"; // Import seeder for demo purposes
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(<App />);
